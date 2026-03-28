cd87283e5bce03da561a6f37bd33a8c8a13f4313
kuzminklk <kuzminkdk@gmail.com>
2026-03-28 19:04:12 +0300

    Finish Section 1 “ERC20”

--- advanced-foundry/ERC20/script/DeployOpenZeppelinERC20.s.sol
+++ advanced-foundry/ERC20/script/DeployOpenZeppelinERC20.s.sol
@@ -3,15 +3,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { KBYN } from "../src/ManualERC20.sol";
+import { KBYN } from "../src/OpenZeppelinERC20.sol";
 import { Script } from "forge-std/Script.sol";
 
 
-contract DeployManualERC20 is Script {
+contract DeployOpenZeppelinERC20 is Script {
 	function run() external returns (KBYN) {
 		vm.startBroadcast();
 
-		KBYN KBYNContract = new KBYN();
+		KBYN KBYNContract = new KBYN(100 ether);
 
 		vm.stopBroadcast();
 
